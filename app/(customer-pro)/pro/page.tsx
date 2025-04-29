d58dd2463a695917a0289cf19461585c29e74724
Mat04 <stopglobalwarming622@gmail.com>
2025-04-29 17:52:47 +0000

    set

--- app/(customer-pro)/pro/page.tsx
+++ app/(customer-pro)/pro/page.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const ProCustomerPage = () => {
-  return <div>Procustomer</div>;
+  return <div className="text-9xl mt-[40vh] text-gray-500">
+    Procustomer
+  </div>;
 };
 
 export default ProCustomerPage;
